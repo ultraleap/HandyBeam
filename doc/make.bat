6ca96dbf6f4fad17ec4d0599f9f202a2a787e571
Jerzy Dziewierz <jerzy.dziewierz@ultrahaptics.com>
2019-06-08 16:43:07 +0100

    updating the documentation making tools

---
+++ doc/make.bat
@@ -0,0 +1,4 @@
+:: this version does not activate handybeam
+@cls
+@sphinx-build -b html -j 12 -E source build
+
--- doc/makedoc.bat
+++ doc/makedoc.bat
@@ -1,1 +1,2 @@
-sphinx-build -b html -j 12 -E source build
+:: this version activates handybeam envinroment. Use only when needed.
+conda activate handybeam & sphinx-build -b html -j 12 -E source build & conda deactivate
